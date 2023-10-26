225d61574c99de04a137980ae73067ae85aaad3a
samcab28 <samircabrera2528@gmail.com>
2023-10-26 15:22:52 -0600

    restaurantes

--- Proyectos/EntregableP2/ventana.cpp
+++ Proyectos/EntregableP2/ventana.cpp
@@ -1327,6 +1327,9 @@ bool ArbolPais::Existe(int codigo) {
 	
 
 
+
+
+
 // CIUDADES
 class nodoCIU {
 public:
@@ -1815,6 +1818,561 @@ bool ArbolCiudad::ExisteCIU(string codigo) {
 
 
 
+
+
+
+
+//clase ArbolRestaurante
+class nodoRE {
+public:
+    nodoRE(const string &v)
+    {
+        valor = v;
+        siguiente = NULL;
+        anterior = NULL;
+    }
+
+    nodoRE(const string &v, nodoRE *signodoRE)
+    {
+        valor = v;
+        siguiente = signodoRE;
+    }
+
+private:
+    string valor;
+    nodoRE *siguiente;
+    nodoRE *anterior;
+
+    friend class ArbolRestaurante;
+};
+typedef nodoRE *pnodoRE;
+
+class ArbolRestaurante {
+public:
+    ArbolRestaurante() { primero = NULL; }
+    ~ArbolRestaurante();
+
+    void agregarNodoIn(string v);
+    void agregarNodo(string v);
+    void InsertarPos(string v, int pos);
+    bool ArbolVacio() { return primero == NULL; }
+    void Imprimir();
+    void MostrarRE();
+    int cantNodos();
+    void CargarDesdeArchivoRE();
+    string ComprobacionRE(string pais, string ciudad, string nombre);
+    void ModificarNombreRE(string pais, string ciudad, string restaurante, string nombre);
+    void agregarRE(ArbolCiudad & arbolCiudad, string pais, string ciudad, string restaurante, string nombre);
+    bool ExisteRE(string codigo);
+    string MostrarArbolRestaurantesPorArbolCiudad();
+    int stringAEnteroRE(const std::string &cadena);
+    string EncontrarValorMayorPedido();
+    string GuardarInformacionRestaurantes();
+    string ObtenerContenidoComoString();
+    int stringAEnteroCl(const std::string &cadena);
+    void BorrarPorCodigosRE(string pais, string ciudad, string restaurante);
+    void BorrarFinal();
+    void BorrarInicio();
+    
+private:
+    pnodoRE primero;
+};
+
+int ArbolRestaurante::stringAEnteroCl(const std::string &cadena) {
+    int resultado = 0;
+    int multiplicador = 1;
+
+    // Comprueba si la cadena representa un n?mero negativo
+    size_t indice = 0;
+    if (cadena[0] == '-') {
+        multiplicador = -1;
+        indice = 1; // Saltar el signo negativo
+    }
+
+    // Recorre la cadena y construye el n?mero entero
+    for (; indice < cadena.length(); ++indice) {
+        char digito = cadena[indice];
+        if (isdigit(digito)) {
+            int valorDigito = digito - '0';
+            resultado = resultado * 10 + valorDigito;
+        } else {
+            // Manejo de error si la cadena contiene caracteres no num?ricos
+            std::cerr << "Error: La cadena contiene caracteres no num?ricos." << std::endl;
+            return 0;
+        }
+    }
+
+    return resultado * multiplicador;
+}
+
+void ArbolRestaurante::BorrarPorCodigosRE(string pais, string ciudad, string restaurante) {
+    if (ArbolVacio()) {
+        cout << "arbol vacio" << endl;
+        return;
+    }
+
+    string codigosBuscados = pais + ";" + ciudad + ";" + restaurante;
+    pnodoRE aux = primero;
+    pnodoRE nodoAEliminar = NULL;
+    bool encontrado = false;
+    int i = 0;
+
+    while (i <= cantNodos()) {
+        if (aux->valor.find(codigosBuscados) != string::npos) {
+            encontrado = true;
+            nodoAEliminar = aux;
+            break;
+        }
+        aux = aux->siguiente;
+        i++;
+    }
+
+    if (encontrado) {
+        if (nodoAEliminar == primero) {
+            BorrarInicio();
+        } else if (nodoAEliminar->siguiente == primero) {
+            BorrarFinal();
+        } else {
+            nodoAEliminar->anterior->siguiente = nodoAEliminar->siguiente;
+            nodoAEliminar->siguiente->anterior = nodoAEliminar->anterior;
+            delete nodoAEliminar;
+        }
+
+        cout << "nodoRE con los c?digos " << codigosBuscados << " borrado exitosamente." << endl;
+    } else {
+        cout << "No encontrado" << endl;
+    }
+}
+
+
+void ArbolRestaurante::BorrarFinal()
+{
+    if (ArbolVacio())
+      cout << "No hay elementos en la lista:" << endl;
+    else
+    {
+      if (primero->siguiente == primero)
+      {
+        pnodoRE temp= primero;
+        primero= NULL;
+        delete temp;
+      }
+      else 
+      {
+         pnodoRE aux = primero; //1
+         while (aux->siguiente->siguiente != primero)
+              aux = aux->siguiente;
+         pnodoRE temp = aux->siguiente;//2
+         aux->siguiente= primero;//3
+         primero->anterior=aux;//4
+         delete temp;//5
+      }
+    }
+}
+
+void ArbolRestaurante::BorrarInicio()
+{
+    if (ArbolVacio())
+      cout << "No hay elementos en la lista:" << endl;
+    else
+    {
+     if (primero->siguiente == primero)
+     {
+        pnodoRE temp= primero;
+        primero= NULL;
+        delete temp;
+     }
+     else
+     {
+        pnodoRE aux = primero->anterior;//1
+        pnodoRE temp= primero;//2
+        aux->siguiente=primero->siguiente;//3
+        primero=primero->siguiente; //4
+        primero->anterior=aux;//5
+        delete temp;//6
+      }
+    }
+}
+
+
+string ArbolRestaurante::ObtenerContenidoComoString() {
+    if (ArbolVacio()) {
+        return "Arbol vacio";
+    }
+
+    std::stringstream ss;
+    pnodoRE aux = primero;
+
+    do {
+        ss << aux->valor << " -> ";
+        aux = aux->siguiente;
+    } while (aux != primero);
+
+    return ss.str();
+}
+
+string ArbolRestaurante::GuardarInformacionRestaurantes() {
+    string memoria;
+
+    if (ArbolVacio()) {
+        cout << "El árbol de Restaurantes está vacío." << endl;
+        return "Árbol vacío";
+    }
+
+    pnodoRE aux = primero;
+    int i = 0;
+
+    while (i < cantNodos()) {
+        memoria += aux->valor + "->";
+        aux = aux->siguiente;
+        i++;
+    }
+
+    // Agregar el último elemento (para que no quede un "; //" extra al final)
+    memoria += aux->valor;
+
+    return memoria;
+}
+
+string ArbolRestaurante::EncontrarValorMayorPedido() {
+    if (ArbolVacio()) {
+        cout << "El arbol de Restaurantes esta vacio." << endl;
+        return "lista vacia";
+    }
+
+    pnodoRE aux = primero;
+    int valorMayor = -1;  
+    pnodoRE nodoMayor = NULL;  
+
+    while (aux->siguiente != primero) {
+        size_t posicionUltimoPuntoComa = aux->valor.find_last_of(';');
+        string pedidoStr = aux->valor.substr(posicionUltimoPuntoComa + 1);
+        int numeroPedido = stringAEnteroRE(pedidoStr);
+
+        if (numeroPedido > valorMayor) {
+            valorMayor = numeroPedido;
+            nodoMayor = aux;
+        }
+
+        aux = aux->siguiente;
+    }
+
+    // Comprobar el ?ltimo nodo tambi?n
+    size_t posicionUltimoPuntoComa = aux->valor.find_last_of(';');
+    string pedidoStr = aux->valor.substr(posicionUltimoPuntoComa + 1);
+    int numeroPedido = stringAEnteroRE(pedidoStr);
+
+    if (numeroPedido > valorMayor) {
+        valorMayor = numeroPedido;
+        nodoMayor = aux;
+    }
+
+    if (nodoMayor != NULL) {
+        cout << "Informacion del Restaurante: " << nodoMayor->valor << endl;
+        return "Informacion del Restaurante: " + nodoMayor->valor;
+    } else {
+        cout << "No se encontr? ning?n Restaurante con con consultas." << endl;
+    }
+}
+
+string ArbolRestaurante::MostrarArbolRestaurantesPorArbolCiudad() {
+    cout << "Digite el codigo de pais: ";
+    string codigoPais;
+    cin >> codigoPais;
+
+    cout << "Digite el codigo de Ciudad: ";
+    string codigoArbolCiudad;
+    cin >> codigoArbolCiudad;
+
+    string codigosBuscados = codigoPais + ";" + codigoArbolCiudad;
+    
+    string memoria;
+    if (ArbolVacio()) {
+        cout << "El arbol de Restaurantes esta vacio." << endl;
+        return "arbol vacia";
+    }
+
+    pnodoRE aux = primero;
+    int i = 0;
+
+    while (i <= cantNodos()) {
+        if (aux->valor.find(codigosBuscados) != string::npos) {
+        	memoria += aux -> valor + "; // ";
+            cout << aux->valor << endl;
+        }
+        aux = aux->siguiente;
+        i++;
+    }
+    return memoria;
+}
+
+void ArbolRestaurante::agregarRE(ArbolCiudad & arbolCiudad, string pais, string ciudad, string restaurante, string nombre){
+    int codigo11 = stringAEnteroCl(pais);
+    int codigo21 = stringAEnteroCl(ciudad);
+    
+
+    std::stringstream ss1, ss2;
+    ss1 << codigo11;
+    ss2 << codigo21;
+
+    string num1 = ss1.str();
+    string num2 = ss2.str();
+
+    string codigosBuscados = num1 + ";" + num2;
+    
+    
+	if(arbolCiudad.ExisteCIU(codigosBuscados)){
+		string NombreNuevo = nombre;
+	
+		string NuevoValor = codigosBuscados + ";" + restaurante + ";" + NombreNuevo +";" +"0";
+		agregarNodo(NuevoValor);
+	
+	}else{
+		cout<<"el pais no existe, No se agrega"<<endl;
+	}
+}
+
+void ArbolRestaurante::ModificarNombreRE(string pais, string ciudad, string restaurante, string nombre) {
+    if (ArbolVacio()) {
+        cout << "El arbol esta vacio" << endl;
+        return;
+    }
+
+    string codigosBuscados = pais +";" + ciudad +";" + restaurante; 
+    pnodoRE aux = primero;
+    bool encontrado = false;
+    int i = 0;
+
+    while (i <= cantNodos()) {
+        if (aux->valor.find(codigosBuscados) != string::npos) {
+            encontrado = true;
+			string modificar = codigosBuscados + ";" + nombre + ";" + "0";
+			aux -> valor = modificar;
+            break;
+        }
+        aux = aux->siguiente;
+        i ++;
+    }
+
+    if (encontrado == false) {
+        cout << "No se encontraron los codigos en el arbol." << endl;
+    }
+
+}
+
+int ArbolRestaurante::stringAEnteroRE(const std::string &cadena) {
+    int resultado = 0;
+    int multiplicador = 1;
+
+    size_t indice = 0;
+    if (cadena[0] == '-') {
+        multiplicador = -1;
+        indice = 1; // Saltar el signo negativo
+    }
+
+    for (; indice < cadena.length(); ++indice) {
+        char digito = cadena[indice];
+        if (isdigit(digito)) {
+            int valorDigito = digito - '0';
+            resultado = resultado * 10 + valorDigito;
+        } else {
+            // Manejo de error si la cadena contiene caracteres no num?ricos
+            std::cerr << "Error: La cadena contiene caracteres no num?ricos." << std::endl;
+            return 0;
+        }
+    }
+
+    return resultado * multiplicador;
+}
+
+string ArbolRestaurante::ComprobacionRE(string pais, string ciudad, string nombre) {
+    if (ArbolVacio()) {
+        cout << "El arbol esta vacio" << endl;
+        return "";
+    }
+
+
+    string codigosBuscados = pais + ";" + ciudad + ";" + nombre;
+    pnodoRE aux = primero;
+    bool encontrado = false;
+    int i = 0;
+
+    while (i <= cantNodos()) {
+        if (aux->valor.find(codigosBuscados) != string::npos) {
+            encontrado = true;
+            size_t posicionUltimoPuntoComa = aux->valor.find_last_of(';');
+            if (posicionUltimoPuntoComa != string::npos) {
+                size_t posicionNumero = posicionUltimoPuntoComa + 1;
+                std::string numeroStr = aux->valor.substr(posicionNumero);
+                int numero = stringAEnteroRE(numeroStr);
+                numero++;
+                
+				std::stringstream ss1;
+    			ss1 << numero;
+
+				string num1 = ss1.str();
+    
+    
+                string nuevoValor = aux->valor.substr(0, posicionNumero) + num1;
+                aux->valor = nuevoValor;
+				return aux->valor;
+            } else {
+                cout << "No se encontr? el ?ltimo punto y coma en el valor." << endl;
+            }
+            break;
+        }
+        aux = aux->siguiente;
+        i++;
+    }
+
+    if (!encontrado) {
+        cout << "No se encontraron los c?digos en el arbol." << endl;
+        return "";
+    }
+}
+
+void ArbolRestaurante::CargarDesdeArchivoRE() {
+    set<string> numero3_set;  
+
+    ifstream archivo("Restaurantes.txt");
+    if (archivo.is_open())
+    {
+        string linea;
+        while (getline(archivo, linea))
+        {
+            istringstream iss(linea);
+            string numero1, numero2, numero3, nombre;
+            getline(iss, numero1, ';');
+            getline(iss, numero2, ';');
+            getline(iss, numero3, ';');
+            getline(iss, nombre);
+
+            // Verificar si NUMERO3 es diferente de los anteriores
+            if (numero3_set.find(numero3) == numero3_set.end())
+            {
+                numero3_set.insert(numero3);  // Agregar a conjunto de NUMERO3
+                string nuevo_valor = numero1 + ";" + numero2 + ";" + numero3 + ";" + nombre + +";"+"0";
+                agregarNodo(nuevo_valor);
+            }
+            else
+            {
+                cout << "Advertencia: NUMERO3 debe ser diferente a los otros NUMERO3 anteriores." << endl;
+            }
+        }
+        archivo.close();
+    }
+    else
+    {
+        cout << "No se pudo abrir el archivo." << endl;
+    }
+}
+                
+ArbolRestaurante::~ArbolRestaurante()
+{
+   pnodoRE aux;
+   pnodoRE temp;
+   
+   while(primero) {
+      temp = primero;
+      aux=primero;
+      primero = primero->siguiente;
+      while (aux->siguiente!=primero)
+           aux= aux->siguiente;
+      aux->siguiente=primero;
+      
+      delete temp;
+      primero=NULL;
+   }
+   primero= NULL;
+}
+                           
+int ArbolRestaurante::cantNodos() 
+{
+    int cont=0;
+
+    pnodoRE aux = primero->siguiente;
+    if(ArbolVacio())
+    {
+        return cont;
+    }
+    else
+    {   cont=cont+1;
+        while(aux!=primero)
+        {
+          aux=aux->siguiente;
+          cont++;
+        }
+    return cont;
+    }
+    
+}
+ 
+void ArbolRestaurante::agregarNodo(string v)
+{
+   if (ArbolVacio())
+     {
+     primero = new nodoRE(v);
+     primero->anterior=primero;
+     primero->siguiente=primero;
+   }  
+   else
+   { 
+     pnodoRE nuevo = new nodoRE(v);//1
+     nuevo->anterior = primero->anterior;//2
+     /*nuevo->siguiente=primero->anterior->siguiente;opcion para intruccion 3*/
+	 nuevo->siguiente=primero;// coloca alguna de la dos 3
+     primero->anterior->siguiente=nuevo;//4
+     primero->anterior=nuevo;//5
+    }    
+}
+
+
+void ArbolRestaurante::MostrarRE()
+{
+   pnodoRE aux=primero;
+   while(aux->siguiente!=primero)
+     {
+                                
+      cout << aux->valor << "-> ";
+      aux = aux->siguiente;
+     }
+     cout<<aux->valor<<"->";
+     cout<<endl;
+
+} 
+
+bool ArbolRestaurante::ExisteRE(string codigo) {
+    if (ArbolVacio()) {
+        cout << "El arbol esta vacio" << endl;
+        return false;
+    }
+    
+    pnodoRE aux = primero;
+    bool encontrado = false;
+    int i = 0;
+
+    while (i <= cantNodos()) {
+        if (aux->valor.find(codigo) != string::npos) {
+            encontrado = true;
+            size_t posicionUltimoPuntoComa = aux->valor.find_last_of(';');
+            string nombre = aux->valor.substr(posicionUltimoPuntoComa + 1);
+            return true;
+            break;
+        }
+        aux = aux->siguiente;
+        i ++;
+    }
+
+    if (!encontrado) {
+        return false;
+    }
+}
+
+
+
+
+
+
 	
 	int main(){
 		//clientes ok 
@@ -1822,51 +2380,47 @@ bool ArbolCiudad::ExisteCIU(string codigo) {
 		ArbolPais ap;
 		administrador ad;
 		ArbolCiudad aCiu;
+		ArbolRestaurante are;
 		
 		ap.CargarDesdeArchivo();
 		
 		ac.CargarDesdeArchivoCl();
 		
-		ac.MostrarCl();
+		ad.CargarDesdeArchivoAd();
 		
+		aCiu.CargarDesdeArchivoCIU();
+		
+		/*
+		//clientes
+		ac.MostrarCl();
 		if(ac.ExisteCl("23242")){
 			cout<<"si esta en la lista"<<endl;
 		}
 		else{
 			cout<<"no esta en la lista"<<endl;
 		}
-		
 		ac.AgregarClienteCl("12345","Samir Cabrera");
-		
 		ac.MostrarCl();
-		
 		cout<<ac.ConsultarClientePorCodigoCl("12345")<<endl;
-		
 		ac.BorrarPorCodigoCl("eqweq");
 		cout<<ac.ObtenerContenidoComoString()<<endl;
-		
 		ac.ModificarNombreCL("12345","pamela");
 		cout<<ac.ObtenerContenidoComoString()<<endl;
 		
 		
+		//paises
 		cout<<ap.ObtenerContenidoComoString()<<endl;
-		
 		ap.AgregarPais("12345","Samir Cabrera");
-		
 		cout<<ap.ConsultarPaisPorCodigo("12345")<<endl;
-		
 		ap.BorrarPorCodigo("1");
-		
 		cout<<ap.ObtenerContenidoComoString()<<endl;
-		
 		ap.ModificarNombre("2", "pamela");
-		
 		cout<<ap.ObtenerContenidoComoString()<<endl;
 		
 		
 		
-		ad.CargarDesdeArchivoAd();
-		
+	
+		//administradores
 		cout<<ad.ObtenerContenidoComoString()<<endl;
 		ad.AgregarAdministradorAd("123456","juanfrancisco");
 		ad.ModificarNombre("657","Mario");
@@ -1875,22 +2429,26 @@ bool ArbolCiudad::ExisteCIU(string codigo) {
 		ad.BorrarPorCodigoAd("657");
 		cout<<ad.ObtenerContenidoComoString()<<endl;
 		
-		aCiu.CargarDesdeArchivoCIU();
+		
 	
-		
+		//ciudades
 		cout<<aCiu.ObtenerContenidoComoString()<<endl;
-		
 		aCiu.AgregarCIU(ap, "1", "34", "tokyo");
-		
 		cout<<aCiu.ObtenerContenidoComoString()<<endl;
-		
 		cout<<aCiu.ComprobacionCIU("1","34")<<endl;
-		
 		aCiu.ModificarNombreCIU("1","34","perez zeledon");
-		
 		aCiu.BorrarPorCodigosCIU("3","89");
+		cout<<aCiu.ObtenerContenidoComoString()<<endl;*/
 		
-		cout<<aCiu.ObtenerContenidoComoString()<<endl;
+		
+		are.CargarDesdeArchivoRE();
+		cout<<are.ObtenerContenidoComoString()<<endl;
+		are.agregarRE(aCiu,"2","35","777","emilio");
+		are.ModificarNombreRE("1","22","125","munchu");
+		cout<<are.ObtenerContenidoComoString()<<endl;
+		cout<<are.ComprobacionRE("1","22","125")<<endl;
+		are.BorrarPorCodigosRE("1","22","125");
+		cout<<are.ObtenerContenidoComoString()<<endl;
 		
 		
 		
@@ -1925,10 +2483,10 @@ bool ArbolCiudad::ExisteCIU(string codigo) {
 	-consultar ||
 	
 	restaurante
-	-agregar 
-	-modificar 
-	-eliminar 
-	-consultar 
+	-agregar ||
+	-modificar ||
+	-eliminar ||
+	-consultar ||
 	
 	menu
 	-agregar 
